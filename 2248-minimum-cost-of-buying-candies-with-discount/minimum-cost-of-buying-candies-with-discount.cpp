a805ac99eeb728b1d6be93bf874fd3e17db05fb1
Kunal Yelgate <kunalyelgatew@gmail.com>
2026-06-01 23:05:26 +0530

    Time: 4 ms (5.99%) | Memory: 16.9 MB (10.30%) - LeetSync

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.rbegin(),cost.rend());
+
+        int candy = 0;
+        for(int i = 0 ; i < cost.size() ; i++) {
+            if( i % 3 != 2) {
+                candy = candy + cost[i];
+            }
+        }
+     return candy;
+    }
+};
