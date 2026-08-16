4f2d2b9701fa91e3126cebd79695e1c4a3b8fcd0
Kunal Yelgate <kunalyelgatew@gmail.com>
2026-08-16 18:35:53 +0530

    Time: 0 ms (100.00%) | Memory: 131.3 MB (69.29%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        
+        int count[3] = {0,0,0};
+
+        for(int k : stones) {
+            count[k % 3]++;
+        }
+
+        if(count[0] % 2 == 0) {
+            return count[1] > 0 && count[2] > 0;
+        }
+
+        return abs(count[1] - count[2]) > 2;
+
+    }
+};
